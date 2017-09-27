83f06729a9bd01184d448cd76d6e2f1a9c74e09a
Jonathan Nacionales <jnaciona@gmail.com>
2017-09-27 02:13:19 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Accord.IO;
+using Accord.MachineLearning;
+using Accord.MachineLearning.Bayes;
+
+namespace dotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+				int[][] inputs =
+				{
+					 //               input      output
+					 new int[] { 0, 1, 1, 0 }, //  0 
+					 new int[] { 0, 1, 0, 0 }, //  0
+					 new int[] { 0, 0, 1, 0 }, //  0
+					 new int[] { 0, 1, 1, 0 }, //  0
+					 new int[] { 0, 1, 0, 0 }, //  0
+					 new int[] { 1, 0, 0, 0 }, //  1
+					 new int[] { 1, 0, 0, 0 }, //  1
+					 new int[] { 1, 0, 0, 1 }, //  1
+					 new int[] { 0, 0, 0, 1 }, //  1
+					 new int[] { 0, 0, 0, 1 }, //  1
+					 new int[] { 1, 1, 1, 1 }, //  2
+					 new int[] { 1, 0, 1, 1 }, //  2
+					 new int[] { 1, 1, 0, 1 }, //  2
+					 new int[] { 0, 1, 1, 1 }, //  2
+					 new int[] { 1, 1, 1, 1 }, //  2
+				};
+
+				int[] outputs = 
+				{
+					 0, 0, 0, 0, 0,
+					 1, 1, 1, 1, 1,
+					 2, 2, 2, 2, 2,
+				};
+
+				// Create a learning algorithm
+				var learner = new NaiveBayesLearning();
+
+				// Teach a model on the data examples
+				NaiveBayes nb = learner.Learn(inputs, outputs);
+
+				// Save and Load model
+				nb.Save(nb, "./naive-bayes.model");
+				NaiveBayes estimator = NaiveBayes.Load("./naive-bayes.model");
+
+				// Output the model 
+				Console.WriteLine(estimator.Decide(new int[] { 0, 1, 1, 0 })); 
+				Console.WriteLine(estimator.Decide(new int[] { 1, 1, 1, 1 })); 
+				Console.WriteLine(estimator.Decide(new int[] { 0, 1, 1, 1 })); 
+				Console.WriteLine(estimator.Decide(new int[] { 1, 1, 1, 0 })); 
+				Console.WriteLine(estimator.Decide(new int[] { 0, 0, 1, 0 })); 
+				Console.WriteLine(estimator.Decide(new int[] { 1, 0, 1, 0 })); 
+				Console.WriteLine(estimator.Decide(new int[] { 1, 1, 0, 1 })); 
+        }
+    }
+}
+
